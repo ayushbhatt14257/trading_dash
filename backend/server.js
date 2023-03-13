9e0c8f2e72030f33d30076ae9e18a95da5a8f921
AyushBhatt <ayushbhatt1425@gmail.com>
2023-03-13 18:22:51 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
+
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,12 @@ app.use(require("./routes/auth"));
 // const DefaultData = require('./defalutData');
 
 const PORT = process.env.PORT || 2000;
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, '/client/build')))
+app.get('*', (req, res) =>
+    res.sendFile(path.join(_dirname, '/client/build/index.html'))
+);
 
 app.listen(PORT, () => {
         console.log(`Server is running ${PORT}`);
